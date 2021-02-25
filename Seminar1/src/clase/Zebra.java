3d5cf95e34f9a65e78f809fe4c03d2b21999097e
Iulian <iuli.nick@yahoo.com>
2021-02-25 13:30:09 +0200

    Commit 3

---
+++ Seminar1/src/clase/Zebra.java
@@ -0,0 +1,21 @@
+package clase;
+
+public class Zebra extends Animal {
+	
+	private int dungi;
+
+	public Zebra(String name) {
+		super(name);
+		this.dungi = 75;
+		// TODO Auto-generated constructor stub
+	}
+
+	public Zebra(String name, int dungi) {
+		super(name);
+		this.dungi = dungi;
+	}
+	
+	
+	
+
+}
--- Seminar1/src/pachet/Main.java
+++ Seminar1/src/pachet/Main.java
@@ -1,6 +1,7 @@
 package pachet;
 
 import clase.Giraffe;
+import clase.Zebra;
 import clase.Zoo;
 
 public class Main {
@@ -10,8 +11,13 @@ public class Main {
 		Giraffe g1 = new Giraffe("bala", 12);
 		Giraffe g2 = new Giraffe("alala");
 		
+		Zebra z1 = new Zebra("tras",34);
+		Zebra z2 = new Zebra("asjh");
+		
 		zoo.adaugaAnimal(g1);
 		zoo.adaugaAnimal(g2);
+		zoo.adaugaAnimal(z1);
+		zoo.adaugaAnimal(z2);
 		zoo.hranesteAnimale();
 	}
 
