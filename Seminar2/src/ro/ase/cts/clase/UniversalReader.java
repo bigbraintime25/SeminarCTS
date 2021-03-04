fb22e3c6d7b5117ab9522044a30c9bfccff44b76
Iulian <iuli.nick@yahoo.com>
2021-03-04 12:21:53 +0200

    Sem 2 commit 2

---
+++ Seminar2/src/ro/ase/cts/clase/UniversalReader.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.clase;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UniversalReader {
+	//TO DO
+//fol ca punct de leg catre readere
+}
